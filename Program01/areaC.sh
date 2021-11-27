92f4ff0fc8bf891e00b29656fd1088231faecf4f
Yogesh T <71491721+Yogesh-T-git@users.noreply.github.com>
2021-11-27 10:27:04 +0530

    Delete areaC.sh

--- Program01/areaC.sh
+++
@@ -1,6 +0,0 @@
-# ! /bin/sh
-
-echo "Enter radius of your circle" 
-read r 
-A=`echo 3.14 \*$r \*$r | bc`           //echo "Area of Circle of radius $r is "
-echo $A                                //echo "3.14 * $r * $r" | bc
