4486d9017e4af818f845dd30ae900e043a5b29b8
Yogesh T <71491721+Yogesh-T-git@users.noreply.github.com>
2021-11-27 11:09:02 +0530

    Update gsal.sh

--- gsal.sh
+++ gsal.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "enter salary"
 read sal
-DA=`expr $sal\*0.1`
+DA=`expr $sal\*0.1`                 //expr wont work on floating poiint no.'s
 HRA=`expr $sal\*0.2`
 echo "gross salary is "
 GS=`echo "$DA+$HRA+$sal" | bc`
