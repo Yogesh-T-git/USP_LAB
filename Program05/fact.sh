6f9b0187854fb5ff2321f459f5686d19dd307b2d
Yogesh T <71491721+Yogesh-T-git@users.noreply.github.com>
2021-11-27 10:58:06 +0530

    Update fact.sh

--- Program05/fact.sh
+++ Program05/fact.sh
@@ -5,7 +5,7 @@ i=1
 fact=1
 while [ $i -le $n ]
 do
-	fact=`echo "$fact * $i" | bc`       
-	i=`echo "$i+1" | bc`                
+	fact=`echo "$fact * $i" | bc`       //since its an integer operation you can also use fact=`expr $fact \* $i`
+	i=`echo "$i+1" | bc`                //i=`expr $i \+ 1`
 done
 echo "Factorial = "$fact
